60249ddc81e119d47c8c2781f896d4eac39b3c72
Lukas <lukas@lukkoeh.io>
2024-12-10 14:57:48 +0100

    chore: update Svelte configuration for prerendering and fallback
    - Set 'fallback' option to '404.html' in svelte.config.js for improved error handling.
    - Added 'prerender' configuration to include all entries for better SEO and performance.

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,10 @@ import adapter from '@sveltejs/adapter-static';
 export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: false
-		})
+			fallback: '404.html'
+		}),
+		prerender: {
+			entries: ['*']
+		}
 	}
 };
